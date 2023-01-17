3b4c6a2a2724b586f6189e4b9230b3f8cd9e70b1
Mary J Hollon <hollonmj1@comcast.net>
2023-01-17 04:30:35 +0000

    added stop words

--- mapper.py
+++ mapper.py
@@ -6,9 +6,20 @@ for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
 
+    # make lower case
+    line = line.lower()
+
     # split the line into words; splits on any whitespace
     words = line.split()
 
+    # output tuples (word,1) in tab -delimited format
+    stopwords = set(['the','and'])
+
     # output tuples (word, 1) in tab-delimited format
     for word in words:
+       if word not in stopwords:
         print '%s\t%s' % (word, "1")
+
+    # This loop is different
+    # We want to exclude the stop words as we read through the line
+    # We also want to add the comma 1 after the word for the reducer.py
